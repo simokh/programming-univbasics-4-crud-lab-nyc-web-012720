aa7184d270075fc8373c12276308355a606148e7
Simo Khouili <m.simokhouili@gmail.com>
2019-12-15 03:39:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,12 +1,15 @@
 def create_an_empty_array
-  
+  []
+end
+create_an_empty_array
+
+def create_an_array 
+  create_an_array = ["hamid", "farid", "john", "frank"]
 end
 
-def create_an_array
-  
-end
 
 def add_element_to_end_of_array(array, element)
+  
   
 end
 
