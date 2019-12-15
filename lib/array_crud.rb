4748e13d1c3851ec6ced9d650db9635b038325eb
Simo Khouili <m.simokhouili@gmail.com>
2019-12-15 03:39:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,38 +3,38 @@ def create_an_empty_array
 end
 create_an_empty_array
 
-def create_an_array 
+def create_an_array
   create_an_array = ["hamid", "farid", "john", "frank"]
 end
 
 
 def add_element_to_end_of_array(array, element)
-  
-  
+
+
 end
 
 def add_element_to_start_of_array(array, element)
-  
+
 end
 
 def remove_element_from_end_of_array(array)
-  
+
 end
 
 def remove_element_from_start_of_array(array)
-  
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+
 end
 
 def retrieve_first_element_from_array(array)
-  
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+
 end
 
 def update_element_from_index(array, index_number, element)
